dfa0379668219944a10eaef17331f7bf3d75c413
ADDICHARLA SATHVIK <85354061+Sathvik-A1901@users.noreply.github.com>
2023-03-23 12:19:20 +0530

    Update video_info.py

--- src/data_extraction/video_info.py
+++ src/data_extraction/video_info.py
@@ -3,9 +3,36 @@ import os
 
 def videos_info(url:str)->list:
     '''
-    In this module we are getting url as input string and we are initializing the options to download the audio of a youtube vedio. To get the audio in the desried 
-    location we are setting up path of current working directory and a folder to store all the audio files. As of now this module can return only the playlist info
-    playlist entries like 
+       Extracting complete information about the playlist
+       
+       Parameters:
+       -----------
+          url: str
+            The youtube plalist url from user
+       
+       returns:
+       --------
+       playlist_entries: list
+          contains all the information about the playlist.That includes:- 
+            ['id', 'title', 'formats', 'thumbnails', 'thumbnail', 'description',
+           'uploader', 'uploader_id', 'uploader_url', 'channel_id', 'channel_url',
+           'duration', 'view_count', 'average_rating', 'age_limit', 'webpage_url',
+           'categories', 'tags', 'playable_in_embed', 'live_status',
+           'release_timestamp', '_format_sort_fields', 'automatic_captions',
+           'subtitles', 'comment_count', 'chapters', 'like_count', 'channel',
+           'channel_follower_count', 'upload_date', 'availability', 'original_url',
+           'webpage_url_basename', 'webpage_url_domain', 'extractor',
+           'extractor_key', 'playlist_count', 'playlist', 'playlist_id',
+           'playlist_title', 'playlist_uploader', 'playlist_uploader_id',
+           'n_entries', 'playlist_index', '__last_playlist_index',
+           'playlist_autonumber', 'display_id', 'fulltitle', 'duration_string',
+           'is_live', 'was_live', 'requested_subtitles', '_has_drm', 'asr',
+           'filesize', 'format_id', 'format_note', 'source_preference', 'fps',
+           'audio_channels', 'height', 'quality', 'has_drm', 'tbr', 'url', 'width',
+           'language', 'language_preference', 'preference', 'ext', 'vcodec',
+           'acodec', 'dynamic_range', 'abr', 'protocol', 'fragments', 'container',
+           'resolution', 'aspect_ratio', 'http_headers', 'audio_ext', 'video_ext',
+           'format']
     '''
     
     #Creating directory to store audio files
