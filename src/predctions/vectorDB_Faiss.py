1b533621b47db1d5c10fc5e23c0d68b8cfd82753
Addicharla Sathvik <addicharlasathvik@gmail.com>
2023-05-06 22:57:20 +0530

    vectorization and searching

---
+++ src/predctions/vectorDB_Faiss.py
@@ -0,0 +1,23 @@
+import faiss
+
+class faiss_db:
+    def __init__(self,d) -> None:
+        #Initializing the index
+        self.index=faiss.IndexFlatL2(d)
+
+        #checking whether the index is trained
+        print(self.index.is_trained)
+
+    def storing_vectors(self,final_emb: list)->None:
+
+        #Adding the embeddings in the index
+        self.index.add(final_emb)
+
+        #Total number of embeddings added
+        print(self.index.ntotal)
+
+    def searching_vectors(self,Xq_emb: str)->list:
+            
+            D, I = self.index.search(Xq_emb,5)
+
+            return I
