cbcf038831edaa42991a529a64267e9b14b4eca1
Addicharla Sathvik <addicharlasathvik@gmail.com>
2023-06-30 15:33:14 +0530

    adding embedding to datafile and modifing existing files

--- src/predctions/pred_main.py
+++ src/predctions/pred_main.py
@@ -3,37 +3,41 @@ from pathlib import Path
 
 from embeddings import gen_embeddings
 from vectorDB_Faiss import faiss_db
+from faiss import write_index
+
+def creating_index(dataframe,path):
+
+    faiss_class=faiss_db(384)
+    
+    
+    embds=gen_embeddings(dataframe['text'])
+    print('embeddings generated')
+
+    faiss_class.storing_vectors(embds)
+    print('Stored successfully')
+
+    write_index(faiss_class.index,str(path))
+
+    return embds
+
 
 
 
 if __name__=='__main__':
     data_path=Path.cwd().parents[1]/'data_files'/'videos_info'/'playlist_info'/'Dataset.csv'
+    index_path=Path.cwd().parents[1]/'data_files'/'videos_info'/'playlist_info'/'Dataset.index'
 
     df=pd.read_csv(data_path)
     print('Data set read completed')
 
-    faiss_class=faiss_db(384)
+    final_embds=creating_index(df,index_path)
+
+    df2=pd.concat([df,pd.DataFrame(final_embds)],axis=1)
+    print(df2.shape)
+
+    df2.to_csv(data_path)
     
-    if faiss_class.index.ntotal!=df.shape[0]:
-        embds=gen_embeddings(df['text'])
-        print('embeddings generated')
-
-        faiss_class.storing_vectors(embds)
-        print('Stored successfully')
     
-    xq= ['what is bootstrap']
-    xq_embd=gen_embeddings(xq)
-    gen_list=faiss_class.searching_vectors(xq_embd)
-    print('searching done')
-
-    lines=df['text']
-    preds=[f'{i}: {lines[i]}' for i in gen_list[0]]
-
-    print(preds)
-
-    urls=[df['url'][i]+'?t='+str(df['start'][i]) for i in gen_list[0]]
-    print(urls)
-
 
 
 
